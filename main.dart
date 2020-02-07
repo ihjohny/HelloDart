98c2a59be5c5267d12a5fe7d0abad0f9d7becc85
ihjohny <ihjony23@gmail.com>
2020-02-07 12:43:14 +0600

    data type and variable

--- main.dart
+++ main.dart
@@ -1,3 +1,26 @@
 void main(){
   print("Hello Word");
+
+  // Data Types and Variables
+  int number = 100;
+  var number1 = 101;
+  print(number);
+  print(number1);
+
+  double number2 = 100.100;
+  var number3 = 100.101;
+  print(number2);
+  print(number3);
+
+
+  bool check = true;
+  var check1 = false;
+  print(check);
+  print(check1);
+
+  String str1 = "String";
+  var str2 = "String using Var";
+  print(str1);
+  print(str2);
+
 }
