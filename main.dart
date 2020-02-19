8e916ec131e8f35b5b87c3b6bd6ad700833b24d7
ihjohny <ihjony23@gmail.com>
2020-02-19 12:53:22 +0600

    for while do while break label in dart.

--- main.dart
+++ main.dart
@@ -1,13 +1,49 @@
 void main() {
 
   // for loop
+  print("for loop example");
   for(int i = 1; i <= 10; i++){
     if(i % 2 == 0)
         print(i);
   }
 
+  print("for each loop example");
   List planetList = ["Mercury", "Venus", "Earth", "Mars"];
   for(String planet in planetList)
       print(planet);
+
+  // while loop
+  print("while loop example");
+  var i = 0;
+  while(i <= 10) {
+    print(i);
+    i++;
+  }
+
+  // do while loop
+  print("do while loop example");
+  var j = 5;
+  do{
+    print(j);
+    j--;
+  }while(j >= 0);
+
+  // break keyword
+  print("break key word example");
+  for(var k = 0; k <= 10; k++){
+    if(k == 5)
+        break;
+    print(k);
+  }
+
+  //using label in for loop
+  loop1: for(var k = 0; k <= 10; k++){
+    for(var l = 0; l <= 10; l++){
+      if(k == 5 && l == 5)
+        break loop1;
+      print("k: $k l: $l");
+    }
+  }
+
 
 }
