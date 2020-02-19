9e4e2cd5b5f0612942b15bcaad620fba8922648b
ihjohny <ihjony23@gmail.com>
2020-02-19 12:53:22 +0600

    for loop in dart

--- main.dart
+++ main.dart
@@ -1,42 +1,13 @@
 void main() {
-  //if else
-  var a = 12;
-  if (a == 10)
-    print('$a is equal to 10');
-  else
-    print('$a is not equal to 10');
 
-  //if else ladder
-  var b = 10;
-  if (b < 10)
-    print('b is less than 10');
-  else if (b > 10)
-    print('b is greater than 10');
-  else
-    print('b is equal to 10');
-
-  //ternary operator
-  a < b ? print("$a is smaller than $b") : print("$b is smaller than $a");
-  var smallerNumber = a < b ? a : b;
-  print(smallerNumber);
-
-  //switch case
-  switch (3) {
-    case 1:
-      print("One");
-      break;
-    case 2:
-      print("Two");
-      break;
-    case 3:
-      print("Three");
-      break;
-    case 4:
-      print("Four");
-      break;
-    default:
-      print("Invalid");
-      break;
+  // for loop
+  for(int i = 1; i <= 10; i++){
+    if(i % 2 == 0)
+        print(i);
   }
+
+  List planetList = ["Mercury", "Venus", "Earth", "Mars"];
+  for(String planet in planetList)
+      print(planet);
 
 }
