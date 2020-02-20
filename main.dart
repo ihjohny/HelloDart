c4dfb3e54fc530c7500813147752214e217bfd99
ihjohny <ihjony23@gmail.com>
2020-02-20 11:45:32 +0600

    function in dart

--- main.dart
+++ main.dart
@@ -1,8 +1,16 @@
 void main() {
-
-  //function in dart without return parameter
-  function(){
-    print("hello");
-  }
+  
   function();
+  print("${function1()}");
+
+}
+
+//function in dart without return parameter
+function(){
+  print("hello");
+}
+
+//function by default return null
+int function1(){
+
 }
