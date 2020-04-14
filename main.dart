f76df2c785a06931de7663bb5bc0154a7b978ba5
Imam Hossain <imam.hossain@brainstation23.com>
2020-04-14 22:33:00 +0600

    Dart Callable Class

--- main.dart
+++ main.dart
@@ -1,26 +1,9 @@
 void main() {
-  //map and hashmap
-  Map<String, int> foods = Map();
-  foods["chicken"] = 2;
-  foods["juice"] = 5;
-  foods["soup"] = 3;
-
-  foods.update("soup", (value) => 99);
-
-  for (String key in foods.keys) {
-    print(key);
+  var one = Person();
+  one("One");
+}
+class Person {
+  call(String name){
+    print("Name is $name");
   }
-
-  for (int value in foods.values) {
-    print(value);
-  }
-
-  Map<String, String> name = {
-    "Don": "one",
-    "Jon": "two",
-    "Kon": "three"
-  };
-
-  name.forEach((key,value) => print(key+" "+value));
-
 }
